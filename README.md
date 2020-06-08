f8f04809af25f9dc88600e6c2db154fa07cb60f7
NikolaVulovic570-2015 <nkl.vulovic96@gmail.com>
2020-06-08 17:03:51 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+My guessing game project! 
+Date and time of creating this file: 
+Fri Jun  8 15:08:12 UTC 2020
+My script has this number of lines: 
+42 guessinggame.sh
---
+++ guessinggame.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+################################################################################
+#Script name	:GUESSING GAME
+#Description	:User try to match number of files.
+#Args		:
+#Author		:Nikola Vulović
+#Email		:nkl.vulovic@gmail.com
+################################################################################
+
+numberOfFiles=$(ls -l | wc -l)
+
+
+Welcome()
+{
+	echo "Welcome to the GuessingGame";
+	echo "How many files are in current directory?";
+}
+
+Match()
+{
+	echo "Great! You are lucky guy!";
+	echo "That's correct!";
+}
+
+Welcome
+
+until [[ $input == $numberOfFiles ]]; 
+do
+read input	
+
+	if [[ $input -lt $numberOfFiles ]]; then
+		echo "The guess is too low"
+		echo "Enter the new guess:"
+		
+	elif [[ $input -gt $numberOfFiles ]]; then
+		echo "The guess is too high"
+		echo "Enter the new guess:"
+		
+	fi
+
+done
+Match
+
+
+
+
+	
+
+
